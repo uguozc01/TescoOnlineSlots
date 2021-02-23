cbd6cbb581df154632ea9767b681523fb2a62e2d
Ugur Ozcan <62249517+uguozc01@users.noreply.github.com>
2021-02-23 23:35:00 +0000

    Update slot_finder.py

--- slot_finder.py
+++ slot_finder.py
@@ -1,8 +1,10 @@
 import os
 import re
 import time
+import selenium
 import smtplib
 from email.message import EmailMessage
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
